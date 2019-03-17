304e2e22af093cd9deb9ccbc433c99f7edccc53b
Daniel Bovington <d.bovington@gmail.com>
2019-03-17 15:27:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
-
 def countdown
+  counter = 1
+  	 
+  while counter <= 5
+  puts "The counter is at: #{counter}"
+  break if counter == 3
+  counter = counter + 1
+  end
+  	 
+  puts "We're done looping!"
+
+
+
 end
