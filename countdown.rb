f0758548c19298a7f86d8897c651adbb89090e15
Daniel Bovington <d.bovington@gmail.com>
2019-03-17 15:31:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,15 +1,10 @@
 #write your code here
 def countdown
   counter = 1
-  	 
-  while counter <= 5
-  puts "The counter is at: #{counter}"
+  while counter <= 10
+  puts "#{counter} 8 to go"
   break if counter == 3
-  counter = counter + 1
+  counter -= 1
   end
-  	 
-  puts "We're done looping!"
-
-
 
 end
